04b79e672ce3fcb424fbdd230445a9398de8b94e
David Chriqui <david.chriqui@outlook.com>
2014-12-31 09:54:27 +0100

    move all controllers

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::SongsController < ApplicationController
+class SongsController < ApplicationController
 
   respond_to :json
 
--- app/controllers/user_playlists_controller.rb
+++ app/controllers/user_playlists_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::UserPlaylistsController < ApplicationController
+class UserPlaylistsController < ApplicationController
   respond_to :json
 
   before_action :authenticate_with_token!, only: [:create, :destroy]
